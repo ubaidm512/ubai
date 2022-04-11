c6a3dcc13c3aa173a0d1e18faa75a6d1bad46387
ubaidm512 <98470646+ubaidm512@users.noreply.github.com>
2022-04-11 22:38:35 +0500

    Add files via upload

---
+++ js js/app.js
@@ -0,0 +1,60 @@
+ 
+//  question no 1
+
+// var Firstname = prompt("Enter First Name");
+// var lastname =  prompt("Enter last Name");
+// alert(Firstname+lastname)
+
+// question no 2
+
+// var string = " samsung galaxy s6 edge plus";
+// var len = string.length;
+// document.write("length of string"+string.length)
+ 
+//  question no 3
+// var user ="pakistan";
+// var indexNumber = user.indexOf("n")
+// document.write("index of 'n' :"  + indexNumber)
+
+
+// question no 4
+
+// var user ="hello world";
+// var indexNumber = user.lastIndexOf("l")
+// document.write(" last index of 'l' :"  + indexNumber)
+
+// questin on 5
+// var user ="pakistani";
+// var indexNumber = user.charAt("3")
+// document.write(" character at index  '3' :"  + indexNumber)
+
+// question no 10
+
+// var text = "peanuts!";
+// var result = text.toUpperCase();
+// document.write(result)
+
+// question no 11
+// var text = "java script!";
+// var result = text.toUpperCase(0);
+// document.write(result)
+
+// question no 12
+// var num = 35.36;
+// var without = num.toString().replace(".", ""); 
+// document.write(without)
+
+
+
+// question no 14
+
+
+// var user = +prompt ["cake", "apple pie", "cookie", "chips", "patties"]
+
+// // "wellcome to abc bakery.what do you want to order sir/ma'am?
+
+
+// question no 17
+var user ="pakistani";
+var indexNumber = user.charAt("7")
+document.write(" character at index  :"  + indexNumber)
